c5d6adc9de1af13322ef1fba1942e0dd1a348758
Alex Junior <silva_jralex@hotmail.com>
2025-01-02 15:07:32 -0300

    refac: refatorei o RequestHandlerWrapper e ISender

--- src/RabbitMQ.Shared.Domain/Contracts/ISender.cs
+++ src/RabbitMQ.Shared.Domain/Contracts/ISender.cs
@@ -2,7 +2,8 @@
 {
     public interface ISender
     {
-        Task<object?> Send(object request, CancellationToken cancellationToken = default);
         Task<TResponse> SendAsync<TResponse>(IRequest<TResponse> request, CancellationToken cancellationToken = default);
+
+        Task SendAsync(IRequest request, CancellationToken cancellationToken = default);
     }
 }
--- src/RabbitMQ.Shared.Domain/Wrappers/RequestHandlerWrapper.cs
+++ src/RabbitMQ.Shared.Domain/Wrappers/RequestHandlerWrapper.cs
@@ -3,18 +3,12 @@ using RabbitMQ.Shared.Contracts.Handler;
 
 namespace RabbitMQ.Shared.Wrappers
 {
-    public abstract class RequestHandlerBase
+    public abstract class RequestHandlerBase<T> 
     {
-        public abstract Task<object?> Handle(object request, IServiceProvider serviceProvider, CancellationToken cancellationToken);
-    }
-
-    public abstract class RequestHandler<T> 
-    {
-        public abstract Task Handle(object request, IServiceProvider serviceProvider, CancellationToken cancellationToken);
         public abstract Task Handle(T request, IServiceProvider serviceProvider, CancellationToken cancellationToken);
     }
 
-    public abstract class RequestHandlerWrapperBase<TResponse> : RequestHandlerBase
+    public abstract class RequestHandlerWrapperBase<TResponse>
     {
         public abstract Task<TResponse> Handle(IRequest<TResponse> request, IServiceProvider serviceProvider,
             CancellationToken cancellationToken);
@@ -28,25 +22,15 @@ namespace RabbitMQ.Shared.Wrappers
             ? throw new InvalidOperationException($"Handler for {typeof(TRequest).Name} not found.")
                 : await handler.Handle((TRequest)request, cancellationToken);
         }
-
-        public override async Task<object?> Handle(object request, IServiceProvider serviceProvider, CancellationToken cancellationToken)
-        {
-            return await Handle((IRequest<TResponse>)request, serviceProvider, cancellationToken);
-        }
     }
 
-    public class RequestHandlerWrapper<TRequest> : RequestHandler<TRequest> where TRequest : IRequest
+    public class RequestHandlerWrapper<TRequest> : RequestHandlerBase<TRequest> where TRequest : IRequest
     {
         public override async Task Handle(TRequest request, IServiceProvider serviceProvider, CancellationToken cancellationToken)
         {
             IRequestHandler<TRequest>? handler = (IRequestHandler<TRequest>?)serviceProvider.GetService(typeof(IRequestHandler<TRequest>)) 
             ?? throw new InvalidOperationException($"Handler for {typeof(TRequest).Name} not found.");
             await handler.Handle(request, cancellationToken);
-        }
-
-        public override async Task Handle(object request, IServiceProvider serviceProvider, CancellationToken cancellationToken)
-        {
-            await Handle((IRequest) request, serviceProvider, cancellationToken);
         }
     }
 
